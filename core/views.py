fce1e7d1461b533b5a484b9f6abaafafc9139aeb
AndriaBeridze <andria24b@gmail.com>
2024-10-05 22:01:57 -0400

    Set up HTML and CSS

--- core/views.py
+++ core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django import http
 
 # Create your views here.
 def home(request):
-    return http.HttpResponse('Hello, World!')
+    return render(request, 'home.html')
